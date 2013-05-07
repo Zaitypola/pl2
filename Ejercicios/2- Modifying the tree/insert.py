dd8165dff11537544f1e9bf96524e9d17276544f
Zaity <jorgecalmar@gmail.com>
2013-05-07 02:48:51 +0200

    windows - inset.py

---
+++ Ejercicios/2- Modifying the tree/insert.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-15 -*-
+from bs4 import BeautifulSoup
+
+'''
+
+insert_before() a certain tag or element
+ insert_after() same as above
+insert() at the end of the parent's contents
+
+'''
