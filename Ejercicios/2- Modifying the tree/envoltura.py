3fa888f9447fa21702afe621285f7ade065caa86
Zaity <jorgecalmar@gmail.com>
2013-05-07 02:13:42 +0200

    windows - cambios en web

--- Ejercicios/2- Modifying the tree/envoltura.py
+++ Ejercicios/2- Modifying the tree/envoltura.py
@@ -7,11 +7,10 @@ Ejercicios:
 '''
 
 from bs4 import BeautifulSoup
-import re
-import urllib2
+
 
 #Envolvemos el p en un nuevo div con id="id2"
-
+'''
 soup = BeautifulSoup('<html><head><title>Title</title></head><body><div id="id1"><p>Hola</p></div><body></html>')
 
 print soup.find(id="id1")
@@ -19,7 +18,21 @@ print soup.find(id="id1")
 tag=soup.find('p').wrap(soup.new_tag("div"))
 tag['id'] = "id2"
 print soup.find(id="id1")
-print tag.name
 
 
+'''
 
+soup = BeautifulSoup("<b></b><p>Hello there</p>")
+tag = soup.b
+tag.append("Hello")
+new_string = soup.new_string(" there")
+tag.append(new_string)
+#print tag
+# <b>Hello there.</b>
+#print tag.contents
+# [u'Hello', u' there']
+print tag.text
+print soup.p.contents
+newstr=soup.new_string(" bitch")
+soup.p.append(newstr)
+print soup.p.contents
