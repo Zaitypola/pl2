f74f40435815f040e840e9e6f9e7ce8a2d13d9fb
Zaity <jorgecalmar@gmail.com>
2013-05-18 01:44:36 +0200

    windows avances gui

--- gui.py
+++ gui.py
@@ -1,20 +1,57 @@
-import pygtk
-import gtk
-import gtk.glade
+# -*- coding: iso-8859-15 -*-
 
-class MainGui:
-    def __init__(self):
-        self.widgets = gtk.glade.XML("noticias.glade")
-        signals = {"search_input_activate" : self.on_search_input_activate,
-                   "search_button_clicked" : self.on_search_input_clicked,
-                   "gtk_main_quit" : gtk.main_quit}
+from Tkinter import *
+
+class newsGUI(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent        
+        self.initUI()
         
-        self.widgets.signal_autoconnect(signals)
-        self.search_input = self.widgets.get_widget("search_input")
-        self.elMundo = self.widgets.get_widget("elMundo_button")
-        self.elPais = self.widgets.get_widget("elPais_button")
-        self.ABC = self.widgets.get_widget("ABC_button")
+    def search(self):
+        print self.var_ABC.get()
+        
+        
+    def initUI(self):
+        categories = [
+                ("Latest news", 0),
+                ("Sports", 1),
+                ("National", 2),
+                ("International", 3),
+            ]      
+        self.parent.title("News feeds searcher")
 
-if __name__== "__main__":
-    MainGui()
-    gtk.main()
+        self.pack(fill=BOTH, expand=1)
+      
+        self.var_elMundo = IntVar()
+        self.var_elPais = IntVar()
+        self.var_ABC = IntVar()
+        self.var_Category = IntVar()
+
+        self.var_Category.set(0)
+        
+        Label(self, text="Categories").pack(anchor=W)
+        for category,value in categories:
+            Radiobutton(self, text=category,variable=self.var_Category, value=value).pack(anchor=W)
+        
+        Label(self, text="Source").place(x=150,y=0)
+        Checkbutton(self, text="El Mundo",variable=self.var_elMundo).place(x=150,y=15)
+        Checkbutton(self, text="El País",variable=self.var_elPais).place(x=150,y=30)
+        Checkbutton(self, text="ABC",variable=self.var_ABC).place(x=150,y=45)
+       
+        Entry(self).place(x=0,y=120)
+        Button(self, text="Search").place(x=130,y=120)
+        Text(self, height=26, width=70).place(x=0,y=150)
+            
+def main():
+    root = Tk()
+    root.geometry("320x240")
+    app = newsGUI(root)
+    root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()  
+ 
