be85ae8bbb26d55db2e7826d84911d00cdc896bf
Zaity <jorgecalmar@gmail.com>
2013-05-18 15:00:01 +0200

    hhjht

---
+++ Ejercicios/1- Navigating/gui.py
@@ -0,0 +1,83 @@
+#!/usr/local/bin/python
+
+from Tkinter import *
+import noticias
+
+
+def search():
+    feeds = []
+    sources = []
+    t.configure(state='normal')
+
+    t.delete(1.0,END)
+
+    if var_ABC.get() == 1:
+        sources.append(abc)
+    if var_elMundo.get() == 1:
+        sources.append(elMundo)
+    if var_elPais.get() == 1:
+        sources.append(elPais)
+        
+    for source in sources:
+        feeds.append(source[var_Category.get()])
+    
+    noticias.search(feeds,var_searchInput.get(),t)
+    t.configure(state='disabled')
+    
+
+categories = [
+        ("Latest news", 0),
+        ("Sports", 1),
+        ("National", 2),
+        ("International", 3),
+    ]      
+
+abc = ['http://www.abc.es/rss/feeds/abc_ultima.xml',
+       'http://www.abc.es/rss/feeds/abc_Deportes.xml',
+       'http://www.abc.es/rss/feeds/abcPortada.xml',
+       'http://www.abc.es/rss/feeds/abc_Internacional.xml']
+
+elMundo= ['http://elmundo.feedsportal.com/elmundo/rss/portada.xml',
+         'http://elmundo.feedsportal.com/elmundodeporte/rss/portada.xml',
+         'http://elmundo.feedsportal.com/elmundo/rss/espana.xml',
+         'http://elmundo.feedsportal.com/elmundo/rss/internacional.xml']
+
+elPais = ['http://ep00.epimg.net/rss/tags/ultimas_noticias.xml',
+           'http://ep00.epimg.net/rss/deportes/portada.xml',
+           'http://ep00.epimg.net/rss/elpais/portada.xml',
+           'http://ep00.epimg.net/rss/internacional/portada.xml']
+
+root = Tk()
+root.geometry("1024x768")
+
+root.title("News feeds searcher")
+frame=Frame(root)
+frame.pack(fill=BOTH, expand=1)
+
+var_elMundo = IntVar()
+var_elPais = IntVar()
+var_ABC = IntVar()
+var_Category = IntVar()
+var_searchInput = StringVar()
+
+var_Category.set(0)
+
+Label(frame, text="Categories").pack(anchor=W)
+
+for category,value in categories:
+    Radiobutton(frame, text=category,variable=var_Category, value=value).pack(anchor=W)
+
+Label(frame, text="Source").place(x=150,y=0)
+Checkbutton(frame, text="El Mundo",variable=var_elMundo).place(x=150,y=15)
+Checkbutton(frame, text="El Pais",variable=var_elPais).place(x=150,y=30)
+Checkbutton(frame, text="ABC",variable=var_ABC).place(x=150,y=45)
+
+t=Text(frame, height=26, width=300)
+t.place(x=0,y=150)
+Entry(frame,textvariable=var_searchInput).place(x=0,y=120)
+Button(frame, text="Search",command=search).place(x=130,y=120)
+
+
+
+
+root.mainloop()
--- Ejercicios/1- Navigating/noticias.py
+++ Ejercicios/1- Navigating/noticias.py
@@ -5,28 +5,23 @@ Cuidado con CDATA - Parsear con "xml"
 from bs4 import BeautifulSoup
 import urllib2
 import re
+from Tkinter import *
 
-feeds = {'http://ep00.epimg.net/rss/elpais/portada.xml',
-         'http://elmundo.feedsportal.com/elmundo/rss/portada.xml',
-         'http://www.abc.es/rss/feeds/abcPortada.xml'
-         }
-while True:
-    search_input = raw_input('Inserte término de búsqueda: ')
+
+def search(feeds,search_input,t):
     for feed in feeds:
         xml = urllib2.urlopen(feed)
         soup = BeautifulSoup(xml,"xml")
-        items = soup.find_all("item") 
-        print '- '+soup.title.text
+        items = soup.find_all("item")
+        t.insert(END,'- '+soup.title.text+'\n')
         for item in items:
             if item.find(["title","description"], text=re.compile(search_input,re.IGNORECASE)):
                 title = item.title.string.encode('cp1252')
                 description = re.sub(r'(<img .+>|<a.+>)','',item.description.string).encode('cp1252')
                 link = item.link.string.encode('cp1252')
                 date = item.pubDate.string.encode('cp1252')
-                print date
-                print title
-                print description
-                print link
-                print '\n'
-            
-    print '\n'
+                t.insert(END,date+'\n')
+                t.insert(END,title+'\n')
+                t.insert(END,description+'\n')
+                t.insert(END,link+'\n')
+    t.insert(END,'\n')
